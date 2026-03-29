92bf7e71b25c68741166440335b467e7546a1c10
pb-league <dougjtucker@gmail.com>
2026-03-29 11:39:20 -0400

    Add files via upload

--- js/settings.js
+++ js/settings.js
@@ -17,8 +17,8 @@
 // const GAS_URL = 'https://script.google.com/macros/s/AKfycbzudYO4IDqCJt92kR4gu6dVJyGN5LoKtxpD3RYR1pNHepxU_liEdpogjCnE8mWTOXqU/exec';
 
 // App version — bump when deploying changes
-const APP_VERSION    = '1.3.2';
-const APP_BUILD_DATE = '2026-03-27';
+const APP_VERSION    = '1.3.3';
+const APP_BUILD_DATE = '2026-03-29';
 
 // Google Analytics 4 Measurement ID
 // Set to your GA4 property ID (format: G-XXXXXXXXXX) to enable analytics.
